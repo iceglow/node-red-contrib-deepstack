0ff4b4815c96c4e5ff40e9848eb2d26a7521d74a
Joakim Lundin <joakim.lundin@bisnode.com>
2021-01-15 12:02:51 +0100

    Server conf in config node to add key credentials

---
+++ deepstack-server.js
@@ -0,0 +1,15 @@
+module.exports = function(RED) {
+    function DeepstackServerNode(n) {
+        RED.nodes.createNode(this,n);
+        this.proto = n.proto;
+        this.host = n.host;
+        this.port = n.port;
+        this.version = n.version;
+        this.rejectUnauthorized = n.rejectUnauthorized;
+    }
+    RED.nodes.registerType("deepstack-server", DeepstackServerNode, {
+        credentials: {
+            apiKey: {type: "text"},
+        }
+    });
+}
